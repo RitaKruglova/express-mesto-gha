374a5ae6347559d459ea839852aef2431944e1a7
Rita Kruglova <kruglova@404@mail.ru>
2023-06-28 20:17:45 +0300

    feat: add get request for /users

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+const User = require('../models/user');
+
+module.exports.getAllUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((error) => res.status(500).send({ error }));
+};
