209de3c5faadba0a61544b5c1a877ba043ac1bda
Rita Kruglova <kruglova404@mail.ru>
2023-06-29 19:18:52 +0300

    feat: add routes patch for /users

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { getAllUsers, getUser, createUser } = require('../controllers/users');
+const {
+  getAllUsers, getUser, createUser, changeAvatar, changeUserInfo,
+} = require('../controllers/users');
 const { doesUserExist } = require('../middlewares/users');
 
 router.get('/', getAllUsers);
 router.get('/:userId', doesUserExist);
 router.get('/:userId', getUser);
 router.post('/', createUser);
+router.patch('/me', changeUserInfo);
+router.patch('/me/avatar', changeAvatar);
 
 module.exports = router;
