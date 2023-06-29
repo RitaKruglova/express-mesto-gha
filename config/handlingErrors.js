1892bdb7756789724f9f1309e2dc1869650b097e
Rita Kruglova <kruglova@404@mail.ru>
2023-06-29 21:17:01 +0300

    fix: change key of error object

--- config/handlingErrors.js
+++ config/handlingErrors.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 module.exports.handleCatch = (err, res) => {
   if (err instanceof mongoose.Error.ValidationError) {
-    res.status(400).send({ error: 'Переданы некорректные данные' });
+    res.status(400).send({ messege: 'Переданы некорректные данные' });
   } else {
-    res.status(500).send({ error: 'Что-то пошло не так...(' });
+    res.status(500).send({ messege: 'Что-то пошло не так...(' });
   }
 };
 
 module.exports.handleThen = (data, res) => {
   if (data === null) {
-    res.status(404).send({ error: 'Объект не найден' });
+    res.status(404).send({ messege: 'Объект не найден' });
   } else {
     res.send({ data });
   }
