24515e9c2cd1349605192951f2e1987030fd0677
Rita Kruglova <kruglova@404@mail.ru>
2023-06-29 15:18:16 +0300

    fix: fix mongoose.connect and set body-parser

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
 const userRouter = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', userRouter);
 
 app.listen(PORT, () => {
