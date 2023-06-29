cc345226c33fd6ab19af56b0d5cbc9880c2c8b37
Rita Kruglova <kruglova404@mail.ru>
2023-06-29 15:26:32 +0300

    feat: add middleware with id

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', userRouter);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '649d764cbd4837b4ec55342c',
+  };
+
+  next();
+});
 
 app.listen(PORT, () => {
   console.log('test');
