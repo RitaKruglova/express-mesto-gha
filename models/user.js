760f6e0e9fe1484d7a3efddcab1c8c4311db32ac
Rita Kruglova <kruglova404@mail.ru>
2023-07-11 19:16:32 +0300

    feat: add email and password in UserSchema

--- models/user.js
+++ models/user.js
@@ -3,17 +3,23 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
     type: String,
     required: true,
   },
